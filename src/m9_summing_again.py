a8164cc5e05384d0f900bbde5abc04956c577992
hueync <hueync@rose-hulman.edu>
2017-11-30 19:17:52 -0500

    DONE

--- src/m9_summing_again.py
+++ src/m9_summing_again.py
@@ -74,7 +74,7 @@ def sum_powers(n, p):
 def run_test_sum_powers_in_range():
     """ Tests the   sum_powers_in_range   function. """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement this function.
+    # DONE: 4. Implement this function.
     #   It TESTS the  sum_powers_in_range  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -91,11 +91,19 @@ def run_test_sum_powers_in_range():
     print('Expected: ', expected)
     print('Actual:   ', actual)
 
-    # Test 1:
-    expected = 135
-    actual = sum_powers_in_range(3, 5, 2)
+    # Test 2:
+    expected = 2989
+    actual = sum_powers_in_range(4, 7, 3)
     print('Expected: ', expected)
     print('Actual:   ', actual)
+
+    # Test 3:
+    expected = 16355625
+    actual = sum_powers_in_range(9, 13, 5)
+    print('Expected: ', expected)
+    print('Actual:   ', actual)
+
+
 
 def sum_powers_in_range(m, n, p):
     """
@@ -110,7 +118,7 @@ def sum_powers_in_range(m, n, p):
       -- sum_powers_in_range(3, 100, 0.1) returns about 142.384776
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #
     #   No fair running the code of  sum_powers_in_range  to GENERATE
